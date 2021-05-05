2cb749a7604368d286acb4099160e1e7dbc58305
Darya Tsimashenkava (@besovadevka) <timaph@mail.ru>
2021-05-05 10:19:16 +0300

    CCCT feat: add possibility to append output file

--- caesar-cipher-cli-tool/caesar/src/helpers/writeInOutputFile.js
+++ caesar-cipher-cli-tool/caesar/src/helpers/writeInOutputFile.js
@@ -4,7 +4,7 @@ const getArgsValue = require("./getArgsValues");
 const writeInOutputFile = (message) => {
   const outputFileName = getArgsValue("-o", "--output");
 
-  fs.writeFile(outputFileName, message, function (error) {
+  fs.appendFile(outputFileName, message, function (error) {
     if (error) {
       process.stdout.write(message);
     }
