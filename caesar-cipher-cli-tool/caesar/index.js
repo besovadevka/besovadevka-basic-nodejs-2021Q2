0f8af26408ba7edb475128a8cbc116b2c194b0e2
Darya Tsimashenkava <70346445+besovadevka@users.noreply.github.com>
2021-10-14 18:15:17 +0300

    Update index.js

--- caesar-cipher-cli-tool/caesar/index.js
+++ caesar-cipher-cli-tool/caesar/index.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
-const program = require("commander");
+const programm = require("commander");
 const { validateRequireArguments } = require("./src/helpers/validation.js");
 const getProcessedData = require("./src/helpers/dataTransformation.js");
 
-program
+programm
   .option("-s --shift <value>", "Shift value for coding")
   .option("-a, --action <value>", "What to do? Encode/decode")
   .option("-i, --input <value>", "An input file")
   .option("-o, --output <value>", "An output file")
   .parse(process.argv);
 
-const { shift, action, input, output } = program.opts();
+const { shift, action, input, output } = programm.opts();
 
 validateRequireArguments(shift, action);
 
